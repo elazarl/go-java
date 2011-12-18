d4357ca9d68a53729dcdf06e0e2c0048a0a5885b
Elazar Leibovich <elazarl@gmail.com>
2011-12-18 18:36:14 +0200

    Cached Thread Pool, to avoid deadlock. I can't schedule threads and replace stacks inside JVM.

--- src/org/golang/runtime/GoRoutines.java
+++ src/org/golang/runtime/GoRoutines.java
@@ -8,8 +8,10 @@ import java.util.concurrent.Executors;
  * Like the Go implementation, goroutines will runWith in a thread pool limited to
  */
 public class GoRoutines {
-    private static final int GOMAXPROCS = Runtime.getRuntime().availableProcessors();
-    static final Executor goroutinesExecutor = Executors.newFixedThreadPool(GOMAXPROCS);
+//    private static final int GOMAXPROCS = Runtime.getRuntime().availableProcessors();
+    // I'm cheating a little, since I can't "steal" the stack of a thread in Java, so I can't have one thread running
+    // two goroutines.
+    static final Executor goroutinesExecutor = Executors.newCachedThreadPool();
 
     public static void go(Runnable goroutine) {
         goroutinesExecutor.execute(goroutine);
